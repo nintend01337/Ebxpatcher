962de66a06823c48a43ab7865262002b2f523597
nintend01337 <horunzhiy11@yandex.ru>
2020-09-30 19:49:30 +0300

    git init cleanup files and fix gitignore

---
+++ EbxPatcher/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EbxPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(593, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(195, 92);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag EBX FILES or Folder here ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Patch it!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(354, 113);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(385, 234);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(403, 162);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "sb file : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EbxPatcher/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EbxPatcher
+{
+    public partial class Form1 : Form
+    {
+        public List<string> ebxfiles = new List<string>();
+        public string SbFilePath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Main knopka   PATCH!
+            Start();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            foreach (string s in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                if (Directory.Exists(s))
+                    ebxfiles.AddRange(Directory.GetFiles(s, "*.ebx", SearchOption.AllDirectories));
+                else
+                    ebxfiles.Add(s);
+
+                richTextBox1.Text = string.Join("\r\n", ebxfiles);
+            }        
+        }
+
+        public void Start()
+        {
+            ebxfiles.ForEach(x =>
+           {
+               readHeader(x);
+           });
+            
+        }
+
+        public  void readHeader(string filename)
+        {
+            var finfo = new FileInfo(filename);
+            byte[] buffer = new byte[finfo.Length];
+
+            buffer = File.ReadAllBytes(filename);
+
+            // Skip 48 bytes Headers and prints GUIDS
+            string guid = BitConverter.ToString(buffer, 48, 16).Replace("-", "");
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+            string pattern = @"\S{1,48}";           // try to convert
+            var result = Regex.Split(guid, pattern);// into 16 byte hex view not succesfull 
+
+            richTextBox2.Text += finfo.Name + "\r\n" + guid + "\r\n \r\n";
+        }
+
+        public void writeGovnoInFile(string file, long offset)
+        {
+            byte[] govno = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255
+        ,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+        255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+        255,255,255,255};
+
+            FileStream fs = new FileStream(file,FileMode.Open);
+
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "SB File(*.sb)| *.sb";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    SbFilePath = dialog.FileName;
+                    label2.Text += " " + dialog.FileName;
+                }
+            }
+        }
+    }
+}
