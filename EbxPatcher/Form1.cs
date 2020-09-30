ca1cb672a00e453985f8bbdad18fa16095ce7e78
nintend01337 <horunzhiy11@yandex.ru>
2020-10-01 01:45:14 +0300

    Added IndexOf finder

--- EbxPatcher/Form1.cs
+++ EbxPatcher/Form1.cs
@@ -48,19 +48,20 @@ namespace EbxPatcher
                     ebxfiles.Add(s);
 
                 richTextBox1.Text = string.Join("\r\n", ebxfiles);
-            }        
+            }
         }
 
         public void Start()
         {
-            ebxfiles.ForEach(x =>
-           {
-               readHeader(x);
-           });
-            
+            // ebxfiles.ForEach(x =>
+            //{
+            //    readHeader(x);
+            //});
+
+            TestMethod();
         }
 
-        public  void readHeader(string filename)
+        public void readHeader(string filename)
         {
             var finfo = new FileInfo(filename);
             byte[] buffer = new byte[finfo.Length];
@@ -69,7 +70,7 @@ namespace EbxPatcher
 
             // Skip 48 bytes Headers and prints GUIDS
             string guid = BitConverter.ToString(buffer, 48, 16).Replace("-", "");
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
             string pattern = @"\S{1,48}";           // try to convert
             var result = Regex.Split(guid, pattern);// into 16 byte hex view not succesfull 
 
@@ -78,14 +79,96 @@ namespace EbxPatcher
 
         public void writeGovnoInFile(string file, long offset)
         {
-            byte[] govno = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255
-        ,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
-        255,255,255,255,255,255,255,255,255,255,255,255,255,255,
-        255,255,255,255};
+            byte[] govno = {255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255};
 
-            FileStream fs = new FileStream(file,FileMode.Open);
+            //FileStream fs = new FileStream(file, FileMode.Open);
 
         }
+
+        //   public int findPos(int pos,byte[] Source, byte[] pattern)
+        public void TestMethod()
+        {
+           int position = 0;
+            string filename1 = @"C:\Users\Glock\Desktop\an94.ebx";
+            string filename2 = @"C:\Users\Glock\Desktop\an94_moded.ebx";
+
+            var finfo1 = new FileInfo(filename1);
+            var finfo2 = new FileInfo(filename2);
+            byte[] zerno = new byte[48];
+            byte[] bytebuffer1 = new byte[finfo1.Length];
+            byte[] bytebuffer2 = new byte[finfo2.Length];
+
+
+            byte[] govno = {255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255,
+                            255,255,255,255,255,255,255,255};
+
+
+            bytebuffer1 = File.ReadAllBytes(filename1);
+            bytebuffer2 = File.ReadAllBytes(filename2);
+
+            for (int i = 0; i < zerno.Length; i++)
+            {
+                zerno[i] = bytebuffer1[i];
+                richTextBox2.Text +=  string.Join(" ",bytebuffer1[i].ToString("x2"));
+            }
+
+
+
+
+            //  position = GetPos(bytebuffer2, zerno);
+
+            position = IndexOf(bytebuffer2, zerno, 0);
+
+            richTextBox2.Text += ($" \n Позиция в файле :  {position} \n");
+
+            if (position > 0)
+            {
+                string findpos = BitConverter.ToString(bytebuffer2, position, 128).Replace("-", "");
+                richTextBox2.Text += findpos;
+                FileStream fs = new FileStream(filename2, FileMode.Open);
+                fs.Seek((long)position, SeekOrigin.Begin);
+                foreach (byte b in govno)
+                {
+                    fs.WriteByte(b);
+                }
+                fs.Close();
+            }
+            
+        }
+
+
+        public int IndexOf(byte[] source, byte[] pattern, int offset)
+        {
+            int success = 0;
+            for (int i = offset; i < source.Length; i++)
+            {
+                if (source[i] == pattern[success])
+                {
+                    success++;
+                }
+                else
+                {
+                    success = 0;
+                }
+
+                if (pattern.Length == success)
+                {
+                    return i - pattern.Length + 1;
+                }
+            }
+            return -1;
+        }
+
+
 
 
         private void label2_Click(object sender, EventArgs e)
