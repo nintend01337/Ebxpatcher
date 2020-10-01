df2be5bb93b0152ef21328c85af0c1a66adc46a2
nintend01337 <horunzhiy11@yandex.ru>
2020-10-01 19:09:19 +0300

    ebx model update

---
+++ EbxPatcher/Classes/Ebx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbxPatcher.Classes
+{
+    public class Ebx
+    {
+        public string path { get; set; }
+        public string head { get; set; }
+        public string guid { get; set; }
+        public byte[] ebxdata;
+        public byte[] bytehead;
+        
+
+        public Ebx(string filename)
+        {
+            path = filename;
+            FileInfo info = new FileInfo(path);
+            ebxdata = new byte[info.Length];
+            bytehead = new byte[64];
+            readData(path);
+            formHeader();
+            setGuid();
+        }
+
+
+        private void readData(string path)
+        {
+            path = this.path;
+            ebxdata = File.ReadAllBytes(path);
+        }
+
+        private void formHeader()
+        {
+            for (int i = 0; i < bytehead.Length; i++)
+            {
+                bytehead[i] = ebxdata[i];
+            }
+                head = BitConverter.ToString(bytehead, 0, 48).ToLower().Replace("-", " ");
+        }
+
+        //private string getStringHeader()
+        //{
+        //    return BitConverter.ToString(bytehead, 0, 48).ToLower().Replace("-", " ");
+        //}
+
+        private void setGuid()
+        {
+           guid =  BitConverter.ToString(bytehead, 48, 64).ToLower().Replace("-", " ");
+        }
+    }
+}
---
+++ EbxPatcher/Classes/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbxPatcher.Classes
+{
+    public static class Extensions
+    {
+        public static int IndexOf(byte[] source, byte[] pattern, int offset)
+        {
+            int success = 0;
+            for (int i = offset; i < source.Length; i++)
+            {
+                if (source[i] == pattern[success])
+                {
+                    success++;
+                }
+                else
+                {
+                    success = 0;
+                }
+
+                if (pattern.Length == success)
+                {
+                    return i - pattern.Length + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
